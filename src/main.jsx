95267b13d7ad7e376ce671f854c89686f82dffc8
Lucas Sánchez <lucasmsanchez@outlook.com>
2026-05-04 12:38:05 -0300

    Configuración ghpages

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import { store } from "./store/store";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
-    <BrowserRouter>
+    <BrowserRouter basename="/mega-ventas">
       <App />
     </BrowserRouter>
   </Provider>
